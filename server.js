d2fb09305c2da5f779f07b68a8bc57ba21454a1d
aaryan2604 <aaryan.dubey@somaiya.edu>
2026-01-28 18:49:37 +0530

    routing and rendering

--- server.js
+++ server.js
@@ -5,13 +5,8 @@ const app=express();
 
 app.listen(3000);
 
-app.set('view engine','ejs');
-
 app.get('/',(req,res)=>{
-    // res.status(200).json({message:"everything is working fine"});
+    res.status(200).json({message:"everything is working fine"});
 
-    // res.send("hello world,i am aaryan dubey");
-
-   res.render('index',{test:"sending data from server"});
+    res.send("hello world,i am aaryan dubey");
 })
-
